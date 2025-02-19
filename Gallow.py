4bfeea8e5830cbc2e0e8f9b790865038b2bc5ccd
Adern <magmed145@gmail.com>
2025-02-19 21:31:59 +0200

    Создал текстовый файл с набор случайных символов - слов. Создал окно для игры с нарисованием висельницы и возможностью ввода букв

--- Gallow.py
+++ Gallow.py
@@ -1,6 +1,11 @@
 from tkinter import *
+import random
 
 main_win = Tk()
+global error
+global my_word
+error = 0
+my_word = []
 
 def ClearWin():
     for widget in main_win.winfo_children():
@@ -11,6 +16,72 @@ def CreateWin():
     main_win.geometry("720x360")
     main_win.resizable(False, False)
     main_win.config(bg="black")
+
+def random_word():
+   with open('russian.txt', 'r') as file:
+        words = file.readlines()
+        words = [s.strip("\n") for s in words]
+        return random.choice(words)
+        
+def GamePlay():
+    ClearWin()
+    word = random_word()
+    i = 0
+
+    canvas = Canvas(bg="white", width=200, height=200)
+    canvas.place(anchor=CENTER, x = 200, y = 160)
+
+    def draw():
+        global error
+        if error == 0:
+            canvas.create_rectangle(5, 200, 200, 190, fill="#80CBC4", outline="#80CBC4")
+            canvas.create_rectangle(165, 190, 180, 30, fill="#80CBC4", outline="#80CBC4")
+            canvas.create_rectangle(165, 30, 95, 45, fill="#80CBC4", outline="#80CBC4")
+            canvas.create_rectangle(95, 45, 110, 60, fill="#80CBC4", outline="#80CBC4")
+
+    def Send():
+        check = False
+        letter = entry_word.get()
+        letter = letter[0]
+        entry_word.delete(0, END)
+        print(letter)
+        i = 0
+        global error
+        while i < len(word):
+            if word[i] == letter:
+                check = True
+                my_word[i] = letter
+            i = i+1
+        if check == True:
+            lbl_word["text"] = my_word
+            return
+        else:
+            error+=1
+            draw()
+        
+    draw()
+    if error == 0:
+        while i < len(word):
+            my_word.append("_")
+            i = i+1
+        print(my_word)
+
+    btn_start = Button(text="✅", width=5, height=2, command=Send)
+    btn_start.place(anchor="c", y=140, x = 500)
+
+    lbl_InputWord = Label(text="Введите букву",bg="black", fg="lime",font=("Arial", 18))
+    lbl_InputWord.place(anchor="c", y=50, x = 500)
+
+    entry_word = Entry(bg="white", justify = CENTER, font=("Arial", 18), width=5)
+    entry_word.place(anchor="c", y=90, x = 500)
+
+    lbl_word = Label(text=my_word,bg="black", fg="lime",font=("Arial", 22))
+    lbl_word.place(anchor="c", y=250, x = 500)
+        
+def Play():
+    error = 0
+    my_word = []
+    GamePlay()
 
 def Info():
     ClearWin()
@@ -43,7 +114,7 @@ def Leave():
 def MainMenu():
     ClearWin()
 
-    btn_start = Button(text="Начать", width=10, height=2)
+    btn_start = Button(text="Начать", width=10, height=2, command=Play)
     btn_start.place(anchor="c",y=120,x=360)
 
     btn_info = Button(text="Информация", width=10, height=2, command=Info)
